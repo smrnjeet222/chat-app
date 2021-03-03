76102563cbb9513ea5eeb3169ea70828d1394cae
Simranjeet Singh <smrnjeet222@gmail.com>
2021-03-04 00:07:33 +0530

    socket logic

--- server/index.js
+++ server/index.js
@@ -1,37 +1,51 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 5000
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
-const router = require('./router')
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(cors());
+// app.use(cors());
 app.use(router);
 
-
-io.on('connection', (socket) => {
-    console.log('We have new connection');
-
+io.on('connect', (socket) => {
     socket.on('join', ({ name, room }, callback) => {
-        try {
-            console.log(name, room);
+        const { error, user } = addUser({ id: socket.id, name, room });
 
-        } catch (error) {
-            callback({ error })
+        if (error) return callback(error);
+
+        socket.join(user.room);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
         }
-
     })
+});
 
-    socket.on("disconnect", () => {
-        console.log("User has left!!!!!!");
-    })
-})
-
-
-server.listen(PORT, () => console.log(`Server running on PORT:- http://localhost:${PORT}`))
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
